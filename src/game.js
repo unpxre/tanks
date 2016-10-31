241de02c8de36c1386b56d6905e85663b30215e1
sfblq1@o2.pl <sfblq1@o2.pl>
2016-11-01 00:54:49 +0100

    Added canvas / client socket utils be/fe / tanks moving

--- src/game.js
+++ src/game.js
@@ -1,48 +1,125 @@
 "use strict";
 
-let _ 	= require('lodash');
+let _ = require('lodash');
 
-let players = [];
+const MOVE_SIZE = 10;
+const ARENA_SIZE = 700;
+const TANK_SIZE = 50;
 
 class Player {
   constructor(socket) {
     this.socket = socket;
     this.id = socket.id;
     this.hearbeatNum = 0;
+    this.x = 0;
+    this.y = 0;
+    this.name = "NAME";
   }
 
   getPublicPlayer() {
     return {
       socketId: this.socket.id,
-      hearbeatNum: this.hearbeatNum
+      hearbeatNum: this.hearbeatNum,
+      x: this.x,
+      y: this.y,
+      name: this.name
     }
   }
 }
 
+class Game {
+  constructor() {
+    this.MAX_PLAYERS = 5;
+
+    this.players = [];
+
+  }
+
+  addPlayer( socket ) {
+    if ( (this.players.length < this.MAX_PLAYERS) && !_.find( this.players, { socket: socket } ) ) {
+      let player = new Player( socket );
+      this.players.push( player );
+      return player;
+    } else {
+      return false;
+    }
+  }
+
+  removePlayer( socket ) {
+    _.remove( this.players, { socket: socket } );
+  }
+
+  getGameData() {
+      return {
+        players: _.map( this.players, (player) => {
+          return player.getPublicPlayer();
+        } )
+      }
+  }
+
+  getPlayers() {
+    return this.players;
+  }
+}
+
+let game = new Game();
+
 module.exports = {
   run: (io, app) => {
-    console.log('run');
 
     // On connection
 		io.on('connection', (socket)  => {
-
+      let player = undefined;
       socket.on('joinToGame', () => {
-        console.log(`New player ${socket.id} entered`);
-        if ( players.length < 5 ) {
-          let player = new Player( socket );
-          players.push( player );
-          socket.emit( 'joinedToGame', { player: player.getPublicPlayer(), someGameData: 'blabla' } );
+        console.log(`New player ${socket.id} connected`);
+        player = game.addPlayer( socket );
+        if ( player ) {
+          socket.emit( 'joinedToGame', { player: player.getPublicPlayer(), gameData: game.getGameData() } );
+          io.emit('newPlayer', { player: player.getPublicPlayer() } );
         }
 			} );
 
 			socket.on('disconnect', ()  => {
 				console.log(`Player ${socket.id} disconnected`);
+        game.removePlayer( socket );
+        io.emit('playerLeft', { playerSocket: socket.id } );
+			} );
+
+      socket.on('move', (data)  => {
+        switch (data) {
+          case 'left':
+          if ( player.x >= MOVE_SIZE ) {
+            player.x -= MOVE_SIZE;
+          }
+          break;
+          case 'down':
+            if ( player.y < ( ARENA_SIZE - TANK_SIZE ) ) {
+              player.y += MOVE_SIZE;
+            }
+            break;
+          case 'up':
+            if ( player.y >= MOVE_SIZE ) {
+              player.y -= MOVE_SIZE;
+            }
+            break;
+          case 'right':
+            if ( player.x < ( ARENA_SIZE - TANK_SIZE ) ) {
+              player.x += MOVE_SIZE;
+            }
+            break;
+        }
+
+        io.emit('playerMoved', {
+          playerSocket: player.socket.id,
+          playerX: player.x,
+          playerY: player.y
+        } );
 			} );
     } );
 
 		// Hearbeat
 		setInterval( () => {
-			_.forEach(players, (player) => {
+			_.forEach(game.getPlayers(), (player) => {
 				++player.hearbeatNum;
 				player.socket.emit( 'hearbeat', {id: player.hearbeatNum, data: 'some data'} );
 			} );
--- src/static/game.js
+++ src/static/game.js
@@ -1,16 +1,96 @@
 
-var socket = io.connect(document.domain);
+let createPlayerObject = (player, stage) => {
+  let image = new Image();
+  image.src = "./static/img/tank1.png";
+  image.onload = function(evt) {
+    let tank = new createjs.Bitmap(evt.target);
+    tank.x = player.x;
+    tank.y = player.y;
+    stage.addChild( tank );
+    player.tank = tank;
+    stage.update();
+  }
+};
 
-socket.on('connect', function () {
-    sessionId = socket.io.engine.id;
-    console.log('Connected ' + sessionId);
-    socket.emit('joinToGame', {});
-});
+let movedPlayerTo = ( player, x, y ) => {
+  createjs.Tween.get(player.tank).to({x: x, y: y}, 100);
+  player.x = x;
+  player.y = y;
+  // console.log('player', player);
+};
 
-socket.on('error', function (reason) {
-   console.log('Unable to connect to server', reason);
-});
+$(document).ready( () => {
+  let socket = io.connect(document.domain);
+  let game = player = undefined;
+  let stage = new createjs.Stage( document.getElementById("gameCanvas") );
 
-socket.on('joinedToGame', function (data) {
-   console.log('Joined to game', data);
-});
+  socket.on('connect', () => {
+      sessionId = socket.io.engine.id;
+      console.log('Connected ' + sessionId);
+      socket.emit('joinToGame', {});
+  });
+
+  socket.on('error', (reason) => {
+     console.log('Unable to connect to server', reason);
+  });
+
+  socket.on('joinedToGame', (data) => {
+     console.log('Joined to game', data);
+     game = data.gameData;
+     player = data.player;
+     console.log('game', game);
+
+     _.forEach( game.players, (player) => {
+       createPlayerObject(player, stage);
+     } );
+  } );
+
+  socket.on('playerMoved', (data) => {
+     let movedPlayer = _.find( game.players, { socketId: data.playerSocket } );
+
+     if ( movedPlayer ) {
+       movedPlayerTo( movedPlayer, data.playerX, data.playerY );
+     }
+  } );
+
+  socket.on('playerLeft', (data) => {
+     let leftPlayer = _.find( game.players, { socketId: data.playerSocket } );
+
+     if ( leftPlayer ) {
+       console.log(`Player (${leftPlayer.socketId}) left game`);
+       stage.removeChild( leftPlayer.tank );
+       _.remove( game.players, leftPlayer );
+     }
+  } );
+
+  socket.on('newPlayer', (data) => {
+    if ( !_.find( game.players, { socketId: data.player.socketId } ) ) {
+      console.log(`New player(${data.player.socketId}) joined to game`);
+      game.players.push( data.player );
+      createPlayerObject( data.player, stage );
+    }
+  } );
+
+  let tick = ()=> {
+    stage.update();
+  }
+
+  createjs.Ticker.addEventListener("tick", tick);
+
+  document.onkeydown = _.throttle( (e) => {
+    switch (e.keyCode) {
+        case 37:
+            socket.emit('move', 'left');
+            break;
+        case 38:
+            socket.emit('move', 'up');
+            break;
+        case 39:
+          socket.emit('move', 'right');
+            break;
+        case 40:
+            socket.emit('move', 'down');
+            break;
+    }
+  }, 100 );
+} );
